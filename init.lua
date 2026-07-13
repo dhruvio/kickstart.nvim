01bfb792694ba52cf2142efb996c87d7cae3ff4c
Dhruv Dang <dhruv@realfolk.com>
2026-07-13 15:39:21 -0700

    Turn on nerd font; Search and grep entire projects files

--- init.lua
+++ init.lua
@@ -99,7 +99,7 @@ do
   vim.g.maplocalleader = ' '
 
   -- Set to true if you have a Nerd Font installed and selected in the terminal
-  vim.g.have_nerd_font = false
+  vim.g.have_nerd_font = true
 
   -- [[ Setting options ]]
   --  See `:help vim.o`
@@ -527,12 +527,28 @@ do
 
   -- See `:help telescope.builtin`
   local builtin = require 'telescope.builtin'
+  local function project_root()
+    return vim.fs.root(0, {
+      '.git',
+      'flake.lock',
+      'devbox.lock',
+      'package-lock.json',
+      'flake.nix',
+      'devbox.json',
+      'package.json'
+    }) or vim.fn.getcwd()
+  end
+
   vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
   vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-  vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+  vim.keymap.set('n', '<leader>sf', function()
+    builtin.find_files { cwd = project_root() }
+  end, { desc = '[S]earch Project [F]iles' })
   vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
   vim.keymap.set({ 'n', 'v' }, '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-  vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+  vim.keymap.set('n', '<leader>sg', function()
+    builtin.live_grep { cwd = project_root() }
+  end, { desc = '[S]earch Project by [G]rep' })
   vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
   vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
   vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
