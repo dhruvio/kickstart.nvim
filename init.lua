9f378db351808122d5e56df5fff71b68dcd5b45d
Dhruv Dang <dhruv@realfolk.com>
2026-07-29 16:28:19 -0700

    Support elm parsing in markdown code blocks

--- init.lua
+++ init.lua
@@ -1003,7 +1003,7 @@ do
   vim.pack.add { { src = gh 'nvim-treesitter/nvim-treesitter', version = 'main' } }
 
   -- Ensure basic parsers are installed
-  local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
+  local parsers = { 'bash', 'c', 'diff', 'elm', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
   require('nvim-treesitter').install(parsers)
 
   ---@param buf integer
