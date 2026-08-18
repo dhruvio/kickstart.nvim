fecc57358e7e930c41b5b253016f8e7c954575bd
Dhruv Dang <dhruv@realfolk.com>
2026-08-17 20:56:14 -0700

    Update markdown preview toggle keybinding

--- lua/custom/plugins/markdown-preview.lua
+++ lua/custom/plugins/markdown-preview.lua
@@ -2,4 +2,4 @@ vim.pack.add { 'https://github.com/iamcco/markdown-preview.nvim' }
 
 vim.g.mkdp_filetypes = { 'markdown' }
 
-vim.keymap.set('n', '<leader>m', '<cmd>MarkdownPreviewToggle<CR>', { desc = 'Toggle [M]arkdown preview' })
+vim.keymap.set('n', '<leader>tm', '<cmd>MarkdownPreviewToggle<CR>', { desc = '[T]oggle [M]arkdown preview' })
