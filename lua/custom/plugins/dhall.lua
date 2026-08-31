7f32a88ed1adcbb03a9dea4626109c8c63d7695e
Dhruv Dang <dhruv@realfolk.com>
2026-08-31 16:35:35 -0700

    Set tabstop to 4 for dhall files

--- lua/custom/plugins/dhall.lua
+++ lua/custom/plugins/dhall.lua
@@ -1,5 +1,14 @@
 vim.lsp.enable 'dhall_lsp_server'
 
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'dhall',
+  callback = function()
+    vim.bo.shiftwidth = 4
+    vim.bo.tabstop = 4
+    vim.bo.softtabstop = 4
+  end,
+})
+
 local conform = require 'conform'
 conform.formatters.dhall = {
   command = 'dhall',
